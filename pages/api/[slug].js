6d4d53d86b71dc00df9dd06d2ba06e85f13e1509
Tanmay Patange <tanmay.patange@gmail.com>
2025-02-21 04:23:56 +0530

    Update [slug].js

--- pages/api/[slug].js
+++ pages/api/[slug].js
@@ -1,23 +1,20 @@
 import { connectToDB } from '../../lib/db'
 
-export default function SlugRedirect() { 
-  return null 
+export default function SlugRedirect() {
+  return null
 }
 
 export async function getServerSideProps(context) {
-  const { slug } = context.params
-  const db = await connectToDB()
-  
-  const entry = await db.collection('urls').findOne({ slug })
-
-  if (!entry) {
+  try {
+    const { slug } = context.params
+    const db = await connectToDB()
+    const entry = await db.collection('urls').findOne({ slug })
+    
+    return entry 
+      ? { redirect: { destination: entry.url, permanent: false } }
+      : { notFound: true }
+  } catch (error) {
+    console.error('Redirect error:', error)
     return { notFound: true }
-  }
-
-  return {
-    redirect: {
-      destination: entry.url,
-      permanent: false
-    }
   }
 }
