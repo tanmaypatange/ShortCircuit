7b8286238985e26e6e8bef0a23867519aa6f27be
Tanmay Patange <tanmay.patange@gmail.com>
2025-02-21 00:23:42 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
-export default function MyApp({ Component, pageProps }) {
+import '../styles/global.css' // Adjust path based on your folder structure
+
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
