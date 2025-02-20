c6fc993498bb23d6cf16df5e191f1667391f95bf
Tanmay Patange <tanmay.patange@gmail.com>
2025-02-21 00:16:50 +0530

    Update mongodb.js

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,18 +1,28 @@
 import { MongoClient } from 'mongodb'
 
 const MONGODB_URI = process.env.MONGODB_URI
+let cachedClient = null
 let cachedDb = null
 
 export async function connectToDB() {
-  if (cachedDb) return { db: cachedDb }
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb }
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI is not defined')
+  }
 
   const client = await MongoClient.connect(MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
 
-  const db = client.db(new URL(MONGODB_URI).pathname.substring(1))
+  const dbName = new URL(MONGODB_URI).pathname.substring(1)
+  const db = client.db(dbName)
+
+  cachedClient = client
   cachedDb = db
 
-  return { db }
+  return { client, db }
 }
